fe42bbb879941d6a3994e9200abfa16b5f26af14
Tyler Witten <turtlewit@live.com>
2017-10-24 13:03:13 -0700

    progress main-menu

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+	
+	public string scene;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	public void Change () {
+		Application.LoadLevel(scene);
+	}
+}
